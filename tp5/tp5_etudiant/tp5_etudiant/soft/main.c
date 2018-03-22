08c39c9a9ee663a94c7eaa0c97af36fa079b3112
Bryan Aggoun <bryan.aggoun@gmail.com>
2018-03-22 22:32:32 +0100

    Add tp5

---
+++ tp5/tp5_etudiant/tp5_etudiant/soft/main.c
@@ -0,0 +1,43 @@
+#include "config.h"
+#include "stdio.h"
+
+#define NPIXEL 256
+#define NLINE  256
+
+//////////////////////////////////////////////////////////////////////
+//	build function
+//////////////////////////////////////////////////////////////////////
+unsigned char build(unsigned int x, unsigned int y, unsigned int step)
+{
+    if( ((x>>step & 0x1) && !(y>>step & 0x1)) ||
+        (!(x>>step & 0x1) && (y>>step & 0x1)) ) 	return 0xFF; 
+    else 						return 0; 
+} // end build
+
+//////////////////////////////////////////////////////////////////////
+//	main function
+/////////////////////////////////////////////////////////////////////
+__attribute__ ((constructor)) void main() 
+{
+    unsigned char 	buf[NPIXEL];
+    int 		n = procid();
+    int			nprocs = NB_PROCS;
+    unsigned int 	line;
+    unsigned int 	pixel;
+
+    for(line = 0 ; line < NLINE ; line++) 
+    { 
+        for(pixel = 0 ; pixel < NPIXEL ; pixel++)
+        {
+            buf[pixel] = build(pixel, line, 5);
+        }
+        if ( fb_sync_write( line*256 , buf, NPIXEL  ) ) 
+            tty_printf(" !!! wrong transfer to frame buffer for line %d\n", line); 
+        else
+            tty_printf(" - building line %d\n", line);
+    }
+
+    tty_printf("\ncycles = %d\n", proctime() );
+    exit(); 
+
+} // end main
